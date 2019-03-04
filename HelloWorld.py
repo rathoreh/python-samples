63f2c32462539aaa0accc0e62731229024e2e1b6
Himanshu Rathore <26323823+rathoreh@users.noreply.github.com>
2019-03-04 22:57:07 +0000

    First program

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+print(">>>>> Learning Python <<<<<\n")
+print("Let's first introduce ourselves")
+print("I'm Python 3.2.7 but you can call me Py3")
+
+name = input("\nTell me your name: ")
+print("Hello ! %s, nice to meet you\n" %name)
+
+print("and with this we have reached the end of our introduction and also learnt how to write a very simple program")
+print("Hope you enjoyed it. Ciao")
