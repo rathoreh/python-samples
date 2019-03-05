e7ec28c130a84f7771651a3e930be6f9870a8fb9
Himanshu Rathore <26323823+rathoreh@users.noreply.github.com>
2019-03-05 21:42:45 +0000

    Simple interest rate calculator featuring sleep(), multi-format print

---
+++ InterestCalculator.py
@@ -0,0 +1,20 @@
+from time import sleep
+
+print(">>>>> Interest Calculator <<<<<")
+print("\n")
+
+principal = float(input('Enter Principal? '))
+roi = float(input("Enter Rate of Interest: "))
+yrs = int(input("No. of Years: "))
+
+print("\nCalculating interest...")
+
+#Sleep is in seconds...
+sleep(1)
+
+maturityAmt = principal * (1 + (roi/100) * yrs) 
+
+interest = maturityAmt - principal
+
+print("\nMaturity amount will be $%0.2f on Principal of $%0.2f for %i years @%0.2f%%" %(maturityAmt, principal, yrs, roi))
+print("earning interest of $%0.2f" %interest)
